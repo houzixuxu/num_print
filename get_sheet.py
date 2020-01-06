8fbfe770f2361ee09f1f50696266c0dcbdd34822
houzixuxu <35063451+houzixuxu@users.noreply.github.com>
2020-01-06 17:42:59 +0800

    Update get_sheet.py

--- get_sheet.py
+++ get_sheet.py
@@ -1,3 +1,10 @@
+import copy
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+from sklearn.metrics import roc_auc_score, roc_curve
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
 class Get_res_DataFrame:
     '''
     sheet1：数据概况
@@ -36,14 +43,15 @@ class Get_res_DataFrame:
         d1 = DataFrame(index=self.use_lst)
         cover_dic = dict(df[use_lst].notnull().sum())
         d1['auc'] = [round(0.5+abs(0.5-roc_auc_score(df[self.y], df[i])), 3) for i in self.use_lst]
-        d1['ks'] = [round(float(ks_calc_cross(df, name, self.y)[0]['gap']), 3) for name in self.use_lst]
-        d1['ins_iv'] = [round(cal_iv(df[df[self.type_train]=='ins'], name, self.y), 3) for name in self.use_lst]
-        d1['oot_iv'] = [round(cal_iv(df[df[self.type_train]=='oot'], name, self.y), 3) for name in self.use_lst]
+        #d1['ks'] = [round(max(abs(roc_curve(df[self.y],df[name])[0]- roc_curve(df[self.y],df[name])[1])), 3) for name in self.use_lst]
+        d1['ks'] = [round(float(self.ks_calc_cross(df, name, self.y)[0]['gap']), 3) for name in self.use_lst]
+        d1['ins_iv'] = [round(self.cal_iv(df[df[self.type_train]=='ins'], name, self.y), 3) for name in self.use_lst]
+        d1['oot_iv'] = [round(self.cal_iv(df[df[self.type_train]=='oot'], name, self.y), 3) for name in self.use_lst]
         
         d1['coef'] = [round(i, 4) for i in self.model.coef_[0]]
         #d1['importance'] = self.model.feature_importances_
         d1 = d1.reset_index()
-        d1['psi'] = [round(cal_psi(df, name), 5) for name in self.use_lst]
+        d1['psi'] = [round(self.cal_psi(df, name), 5) for name in self.use_lst]
         d1['vif'] = [round(variance_inflation_factor(np.matrix(df[self.use_lst]), i),3) for i in range(len(self.use_lst))]
         #d1['fill_missing_data'] = [fill_na_dic[name] for name in self.use_lst]
         #d2_1 = d1
@@ -81,20 +89,17 @@ class Get_res_DataFrame:
             df_tmp = self.df_bin[(self.df_bin[self.type_train]==i)]
 
             for name in self.use_lst:
-                #ks_lst = list(ks_calc_cross(df_tmp, name, self.y)[1]['gap'])
+                #ks_lst = list(self.ks_calc_cross(df_tmp, name, self.y)[1]['gap'])
                 #while  len(ks_lst) > df_tmp.shape[0]:
                 #    ks_lst.pop()
                 #while len(ks_lst) < df_tmp.shape[0]:
                 #    ks_lst.append(0)
-                #print()
+                #print(ks_lst)
                 dd_tmp = df_tmp.groupby(name).sum()[[self.y, 'count']]
                 dd_tmp['bad_rate'] = dd_tmp[self.y]/dd_tmp['count']
                 dd_tmp = dd_tmp.reset_index()
                 dd_tmp['woe'] = dd_tmp[name].apply(lambda x: self.woe_dic[name][x])
-                #dd_tmp['ks']
-                #print(dd_tmp)
-                dd_tmp.sort_values(by='bad_rate', inplace=True)
-                
+                dd_tmp.sort_values(by='bad_rate', inplace=True)        
                 dd_tmp['sort_key'] = [float(i.split(',')[0][1:]) if i[0]=='(' else float('inf') for i in dd_tmp[name]]
                 #print(dd_tmp)
                 dd_tmp.sort_values(by='sort_key', inplace=True)
@@ -108,9 +113,6 @@ class Get_res_DataFrame:
                 else:
                     split_name = str(type_lst[loc-1])+'<-->'+str(i)
                 d[split_name] = [split_name for i in range(d.shape[0])]
-                #d['sort_key'] = [float(i.split(',')[0][1:]) if i[0]=='(' else float('inf') for i in d['slice']]
-                #print(d)
-                #d.sort_values(by='sort_key', inplace=True)
                 d = d[[split_name, 'slice', 'bad', 'count', 'bad_rio', 'woe' ]]                 
                 lst.append(d)
             res.append(lst)  
@@ -160,6 +162,63 @@ class Get_res_DataFrame:
         #
         df.index = range(1, df.shape[0]+1)
         return df
+    def ks_calc_cross(self,data,pred,y_label):
+        '''
+        功能: 计算KS值，输出对应分割点和累计分布函数曲线图
+        输入值:
+        data: 二维数组或dataframe，包括模型得分和真实的标签
+        pred: 一维数组或series，代表模型得分（一般为预测正类的概率）
+        y_label: 一维数组或series，代表真实的标签（{0,1}或{-1,1}）
+        输出值:
+        'ks': KS值，'crossdens': 好坏客户累积概率分布以及其差值gap
+        '''
+        crossfreq = pd.crosstab(data[pred],data[y_label])
+        crossdens = crossfreq.cumsum(axis=0) / crossfreq.sum()
+        crossdens['gap'] = abs(crossdens[0] - crossdens[1])
+        ks = crossdens[crossdens['gap'] == crossdens['gap'].max()]
+        return ks,crossdens
+    
+    def cal_iv(self,df1, x, y='is_7_p'):
+        df = copy.deepcopy(df1)
+        if 'count' not in df.columns:
+            df['count'] = [1 for i in range(df.shape[0])]
+        df_tmp = df[[x,'count', y]].groupby(x).sum()
+        df_tmp['good'] = df_tmp['count'] - df_tmp[y]
+        df_tmp[y] = df_tmp[y].apply(lambda x: max(x, 0.00001)/sum(df_tmp[y]))
+        df_tmp['good'] = df_tmp['good'].apply(lambda x: max(x, 0.00001)/sum(df_tmp['good']))
+        #计算woe
+        df_tmp['woe'] = np.log(df_tmp[y]/df_tmp['good'])
+        #计算iv
+        df_tmp['iv'] = (df_tmp[y]-df_tmp['good']) * df_tmp['woe']
+        return df_tmp['iv'].sum()
+    
+    
+    #计算psi
+    def cal_psi(self, df_sf_bin, name,  lst=['ins', 'oot']):
+        name1, name2 = lst
+        
+        df_in = copy.deepcopy(df_sf_bin[df_sf_bin['type_train']==name1])
+        sum_1 = df_in.shape[0]
+        df_in['count1'] = [1 for i in range(sum_1)]
+        df_in = df_in.groupby(name).sum()[['count1']]
+        
+        df_out = copy.deepcopy(df_sf_bin[df_sf_bin['type_train']==name2])
+        sum_2 = df_out.shape[0]
+        df_out['count2'] = [1 for i in range(sum_2)]
+        df_out = df_out.groupby(name).sum()[['count2']]
+        df_psi = pd.concat((df_in, df_out), axis=1)
+        #计算psi
+        df_psi['count1'] = df_psi['count1'].apply(lambda x: x/sum_1)
+        df_psi['count2'] = df_psi['count2'].apply(lambda x: x/sum_2)
+        #处理出现0的空箱
+        df_psi[['count1', 'count2']].replace(0, 0.001, inplace=True)
+        #
+        df_psi['psi_tmp'] = df_psi['count1']/df_psi['count2']
+        df_psi['psi_tmp'] = df_psi['psi_tmp'].apply(lambda x: math.log(x))
+       # print(df_psi)
+        df_psi['psi'] = (df_psi['count1'] - df_psi['count2'])*df_psi['psi_tmp']
+        #df_psi
+        return sum(df_psi['psi'])
         
 if __name__ == '__main__':
     
@@ -181,3 +240,4 @@ if __name__ == '__main__':
            d6_1 = c.get_categories_df(df_new,cate='month',base_cut='0', y='final_score')
            d6_2 = c.get_categories_df(df_old,cate='month',base_cut='0', y='final_score')
         '''
+    
